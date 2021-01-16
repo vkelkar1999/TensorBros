533a3f298bf87d03594c0fd1f0b2a4ca8f80f92d
vkelkar1999 <vkelkar@ucsd.edu>
2021-01-16 13:22:36 -0800

    Added Initial Inputs

--- FrontEnd/vaci-date/src/App.js
+++ FrontEnd/vaci-date/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, React, useState } from 'react';
 
 
 function App() {
+
+  /*const handleChange = event => {
+    this.setState({ username: event.target.value });
+  }; */
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello</p>
+
+         <label htmlFor="username">Enter Name</label>
+         <input value={name} onInput={e => setName(e.target.value)}/>
+         <label htmlFor="useremail">Enter Email</label>
+         <input value={email} onInput={e => setEmail(e.target.value)}/>
         
       </header>
     </div>
