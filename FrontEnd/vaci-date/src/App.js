0ff2508a5e1dbe075cfb8413f3aff321866bca3f
Justin Leung <justinleung1999@hotmail.com>
2021-01-16 17:46:34 -0800

    Had a wild fiasco with tailwind

--- FrontEnd/vaci-date/src/App.js
+++ FrontEnd/vaci-date/src/App.js
@@ -19,43 +19,63 @@ import { Component, React, useState } from 'react';
     }
   
     handleChange = (event) => {
+      console.log('I was triggered during render')
       this.setState({
         [event.target.name]:event.target.value
       })
-      // const { name, value } = e.target;
-  
-      // this.setState({
-      //   [name]: value
-      // });
     };
-  
+
+    handleSubmit = (event) => { 
+      event.preventDefault();
+      const userData = { 
+        email: this.state.email,
+        house: this.state.house,
+        job: this.state.job
+    };
+      console.log(this.state)
+    };
+    
     render() {
-      return (
-        <div className="radio-buttons">
-          Windows
-          <input
-            value="1"
-            name="age"
-            type="radio"
-            onChange={this.handleChange}
-          />
-          Mac
-          <input
-            value="2"
-            name="age"
-            type="radio"
-            onChange={this.handleChange}
-          />
-          Linux
-          <input
-            value="3"
-            name="age"
-            type="radio"
-            onChange={this.handleChange}
-          />
-          <div>
-          Selected option is : {this.state.age}
-        </div>
+      return (  
+        <div>
+        <form onSubmit={this.handleSubmit}>
+          <div class="mt-4">
+          <div className="radio-buttons">
+            65+
+            <input
+              value="1"
+              name="age"
+              type="radio"
+              onChange={this.handleChange}
+            />
+            55-64
+            <input
+              value="2"
+              name="age"
+              type="radio"
+              onChange={this.handleChange}
+            />
+            18-54
+            <input
+              value="3"
+              name="age"
+              type="radio"
+              onChange={this.handleChange}
+            />
+            Under 18
+            <input
+              value="4"
+              name="age"
+              type="radio"
+              onChange={this.handleChange}
+            />
+            </div> 
+            <div>
+            Selected option is : {this.state.age}
+            </div>
+          </div>
+          <button type="submit">Do the thing</button>
+        </form>
         </div>
       );
     }
