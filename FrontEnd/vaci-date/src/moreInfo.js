066aeaf951c7689e1cbfb4bee7e158b2e05ed54c
Justin Leung <justinleung1999@hotmail.com>
2021-01-16 19:26:15 -0800

    Added basic info to moreInfo

--- FrontEnd/vaci-date/src/moreInfo.js
+++ FrontEnd/vaci-date/src/moreInfo.js
@@ -1,7 +1,38 @@
-function moreInfo() {
-    const greeting = 'Hello Function Component!';
+import {Component, React} from 'react';
+
+
+
+
+class moreInfo extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        email:"",
+        phoneNumber:"",
+      };
+    }
+
+    handleChange(event){
+      this.setState({
+        [event.target.name]:event.target.value
+      })
+    }
+
+    handleSubmit = (event) => { 
+      event.preventDefault();
+      const userData = { 
+        email:this.state.email,
+        phoneNumber:this.state.phoneNumber
+    };
+  }
    
-    return <h1>{greeting}</h1>;
+    render(){
+      return(
+        <div>
+          Hello
+        </div>
+      )
+    }
   }
    
   export default moreInfo;
