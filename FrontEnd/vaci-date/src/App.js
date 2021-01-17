abfc0b318bc3d6b3043da60fd67bb91f371510c7
vkelkar1999 <vkelkar@ucsd.edu>
2021-01-16 16:16:28 -0800

    Pre-state push

--- FrontEnd/vaci-date/src/App.js
+++ FrontEnd/vaci-date/src/App.js
@@ -3,61 +3,128 @@ import './App.css';
 import { Component, React, useState } from 'react';
 
 
-function App() {
 
   /*const handleChange = event => {
     this.setState({ username: event.target.value });
   }; */
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+class App extends Component {
 
-  function App() {
-  return (
-    <div className="App" >
-      <div className="w-full max-w-md bg-gray-800" >
-        <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-          <div className="px-4 pb-4">
-            <label htmlFor="name" className="text-sm block font-bold  pb-2">Name</label>
-            <input type="name" name="name" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="..."/>
-          </div>
-          <div  className="px-4 pb-4">
-            <label htmlFor="age" className="text-sm block font-bold pb-2">Age</label>
-            <input type="age" name="age" id="age" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="..."/>
-          </div>
-          <div className="px-4 pb-4">
-            <label htmlFor="height" className="text-sm block font-bold  pb-2">Height</label>
-            <input type="height" name="height" id="height" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="..."/>
-          </div>
-          <div className="px-4 pb-4">
-            <label htmlFor="weight" className="text-sm block font-bold  pb-2">Weight</label>
-            <input type="weight" name="weight" id="weight" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="..."/>
-          </div>
-          <div className="px-4 pb-4">
-            <label htmlFor="county" className="text-sm block font-bold  pb-2">County</label>
-            <input type="county" name="county" id="county" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="..."/>
-          </div>
-          <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" >Submit</button>
-          </div>
-        </form>
-      </div>
-  </div>
-  );
-}
+  setAge(event) {
+    this.setState({
+      age: event.target.value
+    });
+  }
+  setHousing(event) {
+    this.setState({
+      house: event.target.value
+    });
+  }
+  setJob(event) {
+    this.setState({
+      job: event.target.value
+    });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
+  formSubmit(event) {
+    
+    
+  }
 
-         <label htmlFor="username">Enter Name</label>
-         <input value={name} onInput={e => setName(e.target.value)}/>
-         <label htmlFor="useremail">Enter Email</label>
-         <input value={email} onInput={e => setEmail(e.target.value)}/>
-        
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <form onSubmit={this.formSubmit}>
+        <div onChange={this.setAge.bind(this)}>
+          <label>Age</label>
+          <div></div>
+            <input
+              type="radio"
+              value="2"
+            />
+            65+ 
+          <label>
+            <input
+              type="radio"
+              value="3"
+            />
+            55+ 
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="4"
+            />
+            18+
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="4"
+            />
+            Child
+          </label>
+        </div>
+        <div></div>
+        <div onChange={this.setHousing.bind(this)}>
+          <label>Current Housing</label>
+          <div></div>
+            <input
+              type="radio"
+              value="1"
+            />
+            Nursing Home 
+          <label>
+            <input
+              type="radio"
+              value="3"
+            />
+            Communal Housing 
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="4"
+            />
+            Private Residence
+          </label>
+        </div>
+        <div onChange={this.setJob.bind(this)}>
+          <label>Occupation</label>
+          <div></div>
+            <input
+              type="radio"
+              value="1"
+            />
+            Emergency Medical Provider
+          <label>
+            <input
+              type="radio"
+              value="2"
+            />
+            Health Care Worker
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="3"
+            />
+            Services and Commercial Work
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="4"
+            />
+            Non-contact Job
+          </label>
+        </div>
+        <div></div>
+        <button className="btn btn-default" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
 export default App;
