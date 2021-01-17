771a307f5b826b824d89b2f818313c5f16398c33
vkelkar1999 <vkelkar@ucsd.edu>
2021-01-17 10:56:44 -0800

    i did nerd

--- FrontEnd/vaci-date/src/moreInfo.js
+++ FrontEnd/vaci-date/src/moreInfo.js
@@ -1,45 +1,42 @@
-import {Component, React} from 'react';
+import {Component, React, useState} from 'react';
 import {Form, Card, Button} from 'react-bootstrap';
 
 
-class moreInfo extends Component {
+/*class moreInfo extends Component {
     constructor(props){
       super(props);
       this.state = {
         email:"",
         phoneNumber:"",
+        classification:"",
       };
-    }
+    } */
 
-    handleChange = (event) => {
-      this.setState({
-        [event.target.name]:event.target.value
-      })
-    }
+  function MoreInfo(props) {
+      const [email, setEmail] = useState('');
+      const [number, setNumber] = useState('');
 
-    handleSubmit = (event) => { 
+      const {id} = props.match.params;
+      console.log(id)
+
+   const handleSubmit = (event) => { 
       event.preventDefault();
-      const userData = { 
-        email:this.state.email,
-        phoneNumber:this.state.phoneNumber
-    };
-    console.log(userData)
-  }
+  } 
    
-    render(){
+    
       return(
         <div>
           <Card>
             <Card.Body>
               <Card.Title className="text-center">More Information About you</Card.Title>
-            <Form onSubmit={this.handleSubmit}>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group id="email">
                   <Form.Label className="text-center" style={{width: "100%"}} id="email">Email</Form.Label>
-                  <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleChange} required/>
+                  <Form.Control type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required/>
                 </Form.Group>
                 <Form.Group className="text-center" style={{width: "100%"}} id="phoneNumber">
                   <Form.Label>Phone Number Starting with Country Code</Form.Label>
-                  <Form.Control type="number" name="phoneNumber" maxLength="10" minLength="10"value={this.state.phoneNumber}  onChange={this.handleChange} required/>
+                  <Form.Control type="number" name="phoneNumber" maxLength="10" minLength="10"value={number} onChange={e => setNumber(e.target.value)} required/>
                 </Form.Group>
               <Button className="w-100" type="submit">Submit</Button>
             </Form>
@@ -50,8 +47,7 @@ class moreInfo extends Component {
           
           </div>
         </div>
-      )
+      );
     }
-  }
    
-  export default moreInfo;
+  export default MoreInfo;
