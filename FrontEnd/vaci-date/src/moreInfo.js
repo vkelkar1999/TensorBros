88f06128dcf918778c541b5d9e0ec569176631b0
Justin Leung <justinleung1999@hotmail.com>
2021-01-16 20:38:33 -0800

    Made changes to both pages for forms

--- FrontEnd/vaci-date/src/moreInfo.js
+++ FrontEnd/vaci-date/src/moreInfo.js
@@ -1,5 +1,5 @@
 import {Component, React} from 'react';
-import {Form} from 'react-bootstrap';
+import {Form, Card, Button} from 'react-bootstrap';
 
 
 
@@ -12,7 +12,7 @@ class moreInfo extends Component {
       };
     }
 
-    handleChange(event){
+    handleChange = (event) => {
       this.setState({
         [event.target.name]:event.target.value
       })
@@ -24,14 +24,28 @@ class moreInfo extends Component {
         email:this.state.email,
         phoneNumber:this.state.phoneNumber
     };
+    console.log(userData)
   }
    
     render(){
       return(
         <div>
-          <Form>
-
-          </Form>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-center">More Information About you</Card.Title>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label className="text-center" style={{width: "100%"}} id="email">Email</Form.Label>
+                  <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleChange} required/>
+                </Form.Group>
+                <Form.Group className="text-center" style={{width: "100%"}} id="phoneNumber">
+                  <Form.Label>Phone Number Starting with Country Code</Form.Label>
+                  <Form.Control type="number" name="phoneNumber" maxLength="10" minLength="10"value={this.state.phoneNumber}  onChange={this.handleChange} required/>
+                </Form.Group>
+              <Button className="w-100" type="submit">Submit</Button>
+            </Form>
+            </Card.Body>
+          </Card>
         </div>
       )
     }
